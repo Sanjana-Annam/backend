e1ecdce0bca6172e5a9b3be8108a93b8f493f8d7
sanjana-Annam <sanjanaannam87@gmail.com>
2025-12-01 15:07:01 +0530

    update upload.js

--- upload.js
+++ upload.js
@@ -9,10 +9,11 @@ console.log("Cloudinary ENV =>", process.env.CLOUD_NAME, process.env.CLOUD_API_K
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
 
 // Use MULTER MEMORY STORAGE first
 const storage = multer.memoryStorage();
