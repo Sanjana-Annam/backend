a5fca73f45fa36c18f628492784ef0a3c6879cb4
sanjana-Annam <sanjanaannam87@gmail.com>
2025-12-02 15:05:25 +0530

    OTP deploy fix3333333333

--- emailService.js
+++ emailService.js
@@ -1,33 +1,20 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
 dotenv.config();
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (to, subject, html) => {
   try {
-    console.log("📌 EMAIL_USER =", process.env.EMAIL_USER);
-    console.log("📌 EMAIL_PASS =", process.env.EMAIL_PASS ? "Loaded" : "Not Loaded");
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,           // better success rate than 465
-      secure: false,       // for port 587
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // must be Gmail App Password
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: `"WEEP" <${process.env.EMAIL_USER}>`,
+    const data = await resend.emails.send({
+      from: "WEEP <onboarding@resend.dev>",
       to,
       subject,
       html,
     });
 
-    console.log("📨 OTP Email sent successfully");
-    console.log("Message ID:", info.messageId);
+    console.log("📨 OTP Email sent successfully ✔", data);
   } catch (error) {
-    console.log("❌ Email sending failed:");
-    console.log(error);
+    console.error("❌ Email sending failed:", error);
   }
 };
